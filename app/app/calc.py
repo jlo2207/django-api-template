75fb710145a2ced406f6cd2e79bd3abecce2aad3
jlo2207 <jameslobsenz@gmail.com>
2024-03-09 14:06:31 -0800

    Configure docker-compose and check wait_for_db

---
+++ app/app/calc.py
@@ -0,0 +1,15 @@
+"""
+Calculator functions.
+"""
+
+
+def add(x, y):
+    """ Add x & y and return result"""
+
+    return x + y
+
+
+def subtract(x, y):
+    """ subtract x from y """
+
+    return y-x
